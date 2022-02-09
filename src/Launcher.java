fc30d107a2d82d131cf071a2660c4d25ed49bcfe
anthony.becuwe <anthony.becuwe@epita.fr>
2022-02-09 16:24:13 +0100

    test

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,5 +1,12 @@
-import java.util.Scanner;
-import java.nio.file.Files;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+
 public class Launcher {
     public static void  main(String[] args){
         System.out.print("Bonjour\n");
@@ -39,14 +46,35 @@ public class Launcher {
                 }
                 else if (a.equals("freq"))
                 {
-                    /*
-                    String path = scanner.nextLine();
+                    System.out.println("La Question");
+                    String b = scanner.nextLine();
+                    Path path = Paths.get(b);
                     try {
-                        java.nio.file.Files.
+                        String content = java.nio.file.Files.readString(path);
+                        content = content.toLowerCase(Locale.ROOT);
+                        String[] words = content.split(" ");
+                        List<String> list = Arrays.asList(words);
+                        Map<Object, Integer> frequencyMap = list.stream()
+                                .collect(toMap(
+                                        s -> s, // key is the word
+                                        s -> 1, // value is 1
+                                        Integer::sum));
+                        List<Object> res = list.stream()
+                                .sorted(comparing(frequencyMap::get).reversed()) // sort by descending frequency
+                                .distinct() // take only unique values
+                                .limit(3)   // take only the first 10
+                                .collect(toList()); // put it in a returned list
+                        for(int i=0;i<3;i++)
+                        {
+                            System.out.print(res.get(i)+" ");
+                        }
+                        System.out.println();
+
+                        
                     } catch (Exception e) {
-                        //TODO: handle exception
+                        System.out.println("Unreadable file: " + e.toString());
                     }
-                    */
+                    
                 }
                 else 
                 {
