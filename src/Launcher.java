06c79a6048a09736fb82e9bc66a87b14f4e7a467
anthony.becuwe <anthony.becuwe@epita.fr>
2022-02-09 15:20:35 +0100

    test

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,12 +5,11 @@ public class Launcher {
         System.out.print("Bonjour\n");
         while(true)
         {
-                try (Scanner scanner = new Scanner( System.in )) {
-                    String a = scanner.nextLine();
-                    if (a.equals("quit"))
-                    {
-                        break;
-                    }
+                Scanner scanner = new Scanner( System.in );
+                String a = scanner.nextLine();
+                if (a.equals("quit"))
+                {
+                    break;
                 }
                 System.out.print("Unknown command\n");
         }
