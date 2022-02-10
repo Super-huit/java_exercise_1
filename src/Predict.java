4cd4e1d22b16389975222ff89c435e34625dbb2d
anthony.becuwe <anthony.becuwe@epita.fr>
2022-02-10 16:59:02 +0100

    Part7 test

--- src/Predict.java
+++ src/Predict.java
@@ -1,71 +1,90 @@
-import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
 
-import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
+public class Predict implements Command {
+    private static class Word {
+        private final String word;
+        private final Map<String, Integer> occurences = new HashMap<>();
 
-public class Predict implements Command{
-    public String name(){
+        public void putFollower(String w) {
+            this.occurences.put(w, this.occurences.getOrDefault(w, 0) + 1);
+        }
+
+        public Word(String word) {
+            this.word = word;
+        }
+
+        public String predict() {
+            if (occurences.isEmpty())
+                return null;
+
+            int i = Collections.max(occurences.values());
+
+            List<String> list = this.occurences.keySet().stream().filter(k -> occurences.get(k).equals(i)).toList();
+
+            return list.get(0);
+        }
+    }
+
+    @Override
+    public String name() {
         return "predict";
     }
 
+    @Override
     public boolean run(Scanner sc) {
-        System.out.println("Chemin du fichier a lire :");
-        String pathStr = sc.nextLine();
-        Path path = Paths.get(pathStr);
+        System.out.println("Le fichier:");
+        String content;
         try {
-            String content = java.nio.file.Files.readString(path);
-            System.out.println("Ecrivez un mot :");
-            String userWords = sc.nextLine().toLowerCase(Locale.ROOT);
-            content = content.toLowerCase()
-                    .replaceAll("[.!?,(\\r\\n|\\r|\\n)]", " ")
-                    .replaceAll(" {2}", " ");
-            if (!content.contains(userWords)){
-                System.out.println("le mot n'est pas dans le texte");
-                return false;
-            }
-            String[] listWord = content.split(" ");
-            Map<String, String> wordsFreq = new HashMap<String, String>();
-            for (String word : listWord){
-                if (wordsFreq.get(word) == null) {
-                    wordsFreq.put(word, getNextFreq(word, listWord));
-                }
-            }
-            System.out.print(userWords);
-            for(int i = 0; i < 19 ; i++){
-                System.out.print(" "+wordsFreq.get(userWords));
-                userWords = wordsFreq.get(userWords);
-            }
-        }  catch (IOException e) {
-            System.out.println("Unreadable file: " + e.toString());
+            content = Files.readString(Path.of(sc.nextLine()));
+        } catch (Exception e) {
+            System.err.println("Unreadable file: " + e.getMessage());
+            return false;
         }
-        System.out.println("");
-        return false;
-    }
 
-    public String getNextFreq(String word,String[] content){
-        ArrayList<String> tmp = new ArrayList<>();
-        for (int i = 0;i < content.length;i++){
-            if (content[i].equals(word) && i != content.length - 1){
-                tmp.add(content[i+1]);
+
+        if (content.isBlank())
+            return false;
+
+        content = content.toLowerCase();
+        content = content.replaceAll("[,.!?\\-'\"\t\n]", " ");
+        content = content.replaceAll(" {2}", " ");
+
+        Map<String, Word> words = new HashMap<>();
+
+        String lastWord = Arrays.stream(content.split(" "))
+                .filter(s -> !s.isBlank())
+                .reduce("", (prev, next) -> {
+                    if (!prev.isBlank()) {
+                        words.putIfAbsent(prev, new Word(prev));
+                        words.get(prev).putFollower(next);
+                    }
+                    return next;
+                });
+
+        words.putIfAbsent(lastWord, new Word(lastWord));
+
+        System.out.println("Veuillez maintenant entrer un mot :");
+        String startWord = sc.nextLine();
+        startWord = startWord.toLowerCase();
+
+        if (!words.containsKey(startWord))
+            System.err.println("Le mot n'est pas présent dans le texte !");
+
+        else {
+            List<String> sentence = new ArrayList<>(List.of(startWord));
+            while (sentence.size() < 20) {
+                String nextWord = words.get(sentence.get(sentence.size() - 1)).predict();
+                if (nextWord == null)
+                    break;
+                sentence.add(nextWord);
             }
+
+            System.out.println(String.join(" ", sentence));
         }
-        Map<Object, Integer> frequencyMap = tmp.stream()
-                .collect(toMap(
-                        s -> s, // key is the word
-                        s -> 1, // value is 1
-                        Integer::sum));
-        List<Object> res = tmp.stream()
-                .sorted(comparing(frequencyMap::get).reversed()) // sort by descending frequency
-                .distinct() // take only unique values
-                .limit(1)   // take only the first 1
-                .collect(toList()); // put it in a returned list
-        if (res.size() != 0) {
-            return res.get(0).toString();
-        }
-        return "";
+
+
+        return false;
     }
 }
